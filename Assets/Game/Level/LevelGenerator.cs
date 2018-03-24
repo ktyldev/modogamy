67b287202b106ebcbab172984e8fdbbc0a648e87
Cat Flynn <cathalflynn@hotmail.co.uk>
2018-03-24 20:55:33 +0000

    Start level generation

---
+++ Assets/Game/Level/LevelGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _levelRoot;
+    [SerializeField]
+    private GameObject _parkTile;
+    [SerializeField]
+    private float _offsetRight;
+
+    // Use this for initialization
+    void Start()
+    {
+        var levelLength = 10;
+
+        for (int i = 1; i <= levelLength; i++)
+        {
+            var tile = Instantiate(_parkTile, _levelRoot);
+
+            var translation = -Vector3.right * (i + 1) * _offsetRight;
+
+            tile.transform.Translate(translation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
