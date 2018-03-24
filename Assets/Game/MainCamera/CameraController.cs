6912098af219e875cdcccfba4c73f797e728b7ac
Cat Flynn <cathalflynn@hotmail.co.uk>
2018-03-24 18:36:09 +0000

    Camera movement

---
+++ Assets/Game/MainCamera/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _trackedObject;
+    [SerializeField]
+    [Range(0, 1)]
+    private float _sensitivity;
+
+    private Vector3 _offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        _offset = transform.position - _trackedObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        var targetPosition = _trackedObject.transform.position + _offset;
+        transform.position = Vector3.Lerp(transform.transform.position, targetPosition, _sensitivity);
+        transform.LookAt(_trackedObject);
+    }
+}
