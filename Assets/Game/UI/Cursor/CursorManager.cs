ad1ed8cfa1256480c607381fc50b1ba36543694d
cyndrdev <34170110+cyndrdev@users.noreply.github.com>
2018-03-24 06:57:12 +0000

    bones. bones. bones!!!

---
+++ Assets/Game/UI/Cursor/CursorManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CursorManager : MonoBehaviour {
+    [SerializeField]
+    Texture2D _cursor;
+
+    private CursorMode _cursorMode = CursorMode.Auto;
+
+    void OnEnable()
+    {
+        Cursor.SetCursor(_cursor, Vector2.zero, _cursorMode);
+    }
+
+    void OnDisable()
+    {
+        Cursor.SetCursor(null, Vector2.zero, _cursorMode);
+    }
+}
