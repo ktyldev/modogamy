70f19acc271292a7ce6b118fcc05adda7b5ef932
cyndrdev <34170110+cyndrdev@users.noreply.github.com>
2018-03-24 20:32:25 +0000

    added postFX handler for the app view

---
+++ Assets/Game/MainCamera/PostFXHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PostFXHandler : MonoBehaviour {
+    private PostProcessVolume _volume;
+    private DepthOfField _dof;
+
+    [SerializeField]
+    [Range(.1f, 32f)]
+    private float _normalAperture;
+    [SerializeField]
+    [Range(.1f, 32f)]
+    private float _phoneAperture;
+
+    private float _targetAperture;
+
+    void Start () {
+        _dof = ScriptableObject.CreateInstance<DepthOfField>();
+        _dof.enabled.Override(true);
+        _dof.focusDistance.Override(.1f);
+        _dof.focalLength.Override(7f);
+        _dof.kernelSize.Override(KernelSize.Medium);
+
+        _volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, _dof);
+        _volume.isGlobal = true;
+	}
+	
+
+	void Update () {
+        _targetAperture = (GameController.IsUsingPhone) ? _phoneAperture : _normalAperture;
+
+        _dof.aperture.value = Mathf.Lerp(
+            _dof.aperture.value,
+            _targetAperture,
+            0.2f);
+
+        print(_dof.aperture.value);
+	}
+
+    private void OnDestroy()
+    {
+        RuntimeUtilities.DestroyVolume(_volume, true);
+    }
+}
