06846de94fa35ad80a8b38a4d07bbeb206b2da24
Cat Flynn <cathalflynn@hotmail.co.uk>
2018-03-24 21:15:52 +0000

    Fix basic level generation

--- Assets/Game/Level/LevelGenerator.cs
+++ Assets/Game/Level/LevelGenerator.cs
@@ -9,18 +9,20 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField]
     private GameObject _parkTile;
     [SerializeField]
-    private float _offsetRight;
+    private float _startOffset;
+    [SerializeField]
+    private float _tileLength;
 
     // Use this for initialization
     void Start()
     {
         var levelLength = 10;
 
-        for (int i = 1; i <= levelLength; i++)
+        for (int i = 0; i < levelLength; i++)
         {
             var tile = Instantiate(_parkTile, _levelRoot);
 
-            var translation = -Vector3.right * (i + 1) * _offsetRight;
+            var translation = -Vector3.right * ((i + 0.5f) * _tileLength + _startOffset);
 
             tile.transform.Translate(translation);
         }
