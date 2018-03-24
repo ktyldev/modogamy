38b99c5fe1fe65c9a472d051aaf42949a3531fc9
cyndrdev <34170110+cyndrdev@users.noreply.github.com>
2018-03-24 02:04:02 +0000

    added doggo factory

---
+++ Assets/Game/Dog/DogFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Extensions;
+
+public class DogFactory : MonoBehaviour {
+    public static int minSize = 10;
+    public static int maxSize = 50;
+    public static double lambda = 25;
+    int size;
+
+    int getSize()
+    {
+        // first, do a rough poisson calculation
+        double p = 1.0, L = Math.Exp(-lambda);
+        int k = 0;
+        do
+        {
+            k++;
+            p *= UnityEngine.Random.value;
+        }
+        while (p > L);
+        k--;
+        // now we've got our poisson, let's clamp it
+        return Mathf.Clamp(k, minSize, maxSize);
+    }
+
+    void Awake ()
+    {
+    }
+
+    void Update ()
+    {
+        size = getSize();
+        print(size);
+    }
+}
