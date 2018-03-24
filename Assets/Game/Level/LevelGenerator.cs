e1ebef937d5d1717e9c5f909c0f4f9680726f33d
Cat Flynn <cathalflynn@hotmail.co.uk>
2018-03-24 23:51:39 +0000

    Generate level end-to-end

--- Assets/Game/Level/LevelGenerator.cs
+++ Assets/Game/Level/LevelGenerator.cs
@@ -9,25 +9,42 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField]
     private GameObject _cityParkTransition;
     [SerializeField]
-    private GameObject _parkTile;
+    private GameObject _park;
+    [SerializeField]
+    private GameObject _parkCityTransition;
+    [SerializeField]
+    private GameObject _end;
     [SerializeField]
     private float _startOffset;
     [SerializeField]
     private float _tileLength;
     [SerializeField]
     private int _levelLength;
-
-    private int _tileIndex;
-
-    // Use this for initialization
+    
     void Start()
     {
         for (int i = 0; i < _levelLength; i++)
         {
-            var tile = Instantiate(i == 0 ? _cityParkTransition : _parkTile, _levelRoot);
-
+            GameObject template = null;
+            if (i == 0)
+            {
+                template = _cityParkTransition;
+            }
+            else if (i == _levelLength - 2)
+            {
+                template = _parkCityTransition;
+            }
+            else if (i == _levelLength - 1)
+            {
+                template = _end;
+            }
+            else
+            {
+                template = _park;
+            }
+            
+            var tile = Instantiate(template, _levelRoot);
             var translation = -Vector3.right * ((i + 0.5f) * _tileLength + _startOffset);
-
             tile.transform.Translate(translation);
         }
     }
