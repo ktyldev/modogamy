c377a2bc5f82f25808fd011b3b2b8ff809752264
Cat Flynn <cathalflynn@hotmail.co.uk>
2018-03-25 06:43:06 +0100

    Import flowers

--- Assets/Game/Level/Trees/Trees.cs
+++ Assets/Game/Level/Trees/Trees.cs
@@ -6,15 +6,22 @@ using UnityEngine;
 public class Trees : MonoBehaviour
 {
     [SerializeField]
-    private GameObject[] _templates;
+    private GameObject[] _treeTemplates;
     [SerializeField]
     private int _treeCount;
     [SerializeField]
     private float _treeSeparation;
     [SerializeField]
+    private GameObject[] _flowerTemplates;
+    [SerializeField]
+    private int _flowerCount;
+    [SerializeField]
+    private float _flowerSeparation;
+    [SerializeField]
     private int _failedAttempts;
 
     private List<Vector3> _treeLocations;
+    private List<Vector3> _flowerLocations;
 
     private void Awake()
     {
@@ -28,20 +35,11 @@ public class Trees : MonoBehaviour
     
     private void SpawnTrees()
     {
-        var bounds = GameController.ParkBounds;
         var failures = 0;
         while (failures < _failedAttempts && _treeLocations.Count() < _treeCount)
         {
             // get a location for the tree
-            var x = Random.Range(bounds.min.x, bounds.max.x);
-            var z = Random.Range(bounds.min.z, bounds.max.z);
-
-            var targetLocation = new Vector3
-            {
-                x = x,
-                z = z
-            };
-
+            var targetLocation = GetLocationInPark();
             // check if it's too close to another tree
             if (_treeLocations.Any(p => Vector3.Distance(p, targetLocation) < _treeSeparation))
             {
@@ -50,11 +48,42 @@ public class Trees : MonoBehaviour
             }
 
             _treeLocations.Add(targetLocation);
-            var treeTemplate = _templates[Random.Range(0, _templates.Length)];
+            var treeTemplate = _treeTemplates[Random.Range(0, _treeTemplates.Length)];
 
             // get a random rotation
             var rot = Quaternion.AngleAxis(Random.Range(0, 360), Vector3.up);
             Instantiate(treeTemplate, targetLocation, rot);
         }
+    }
+
+    private void SpawnFlowers()
+    {
+        var failures = 0;
+        while (failures < _failedAttempts && _flowerLocations.Count() < _treeCount)
+        {
+            var targetLocation = GetLocationInPark();
+            if (_flowerLocations.Any(p => Vector3.Distance(p, targetLocation) < _flowerSeparation))
+            {
+                failures++;
+                continue;
+            }
+
+            _flowerLocations.Add(targetLocation);
+            var template = _flowerTemplates[Random.Range(0, _flowerTemplates.Length)];
+
+            // get a random rotation
+            var rot = Quaternion.AngleAxis(Random.Range(0, 360), Vector3.up);
+            Instantiate(template, targetLocation, rot);
+        }
+    }
+
+    private Vector3 GetLocationInPark()
+    {
+        var bounds = GameController.ParkBounds;
+        return new Vector3
+        {
+            x = Random.Range(bounds.min.x, bounds.max.x),
+            z = Random.Range(bounds.min.z, bounds.max.z)
+        };
     }
 }
