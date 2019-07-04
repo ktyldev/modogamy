c05d5b3a64868520955eb28a2823b51d89414c0f
cat flynn <me@ktyl.dev>
2019-07-04 21:40:12 +0100

    remove out of date unity code, new scratch scene

--- Assets/Game/Level/LevelGenerator.cs
+++ Assets/Game/Level/LevelGenerator.cs
@@ -15,36 +15,18 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField]
     private GameObject _end;
     [SerializeField]
-    private float _startOffset;
-    [SerializeField]
     private float _tileLength;
     [SerializeField]
     private int _levelLength;
     
     void Start()
     {
+        float start = -_levelLength * _tileLength;
+
         for (int i = 0; i < _levelLength; i++)
         {
-            GameObject template = null;
-            if (i == 0)
-            {
-                template = _cityParkTransition;
-            }
-            else if (i == _levelLength - 2)
-            {
-                template = _parkCityTransition;
-            }
-            else if (i == _levelLength - 1)
-            {
-                template = _end;
-            }
-            else
-            {
-                template = _park;
-            }
-            
-            var tile = Instantiate(template, _levelRoot);
-            var translation = -Vector3.right * ((i + 0.5f) * _tileLength + _startOffset);
+            var tile = Instantiate(_park, _levelRoot);
+            var translation = Vector3.right * ((i + 0.5f) * _tileLength + start);
             tile.transform.Translate(translation);
         }
     }
--- Assets/Game/MainCamera/PostFXHandler.cs
+++ Assets/Game/MainCamera/PostFXHandler.cs
@@ -1,12 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.PostProcessing;
 using Extensions;
 
 public class PostFXHandler : MonoBehaviour {
-    private PostProcessVolume _volume;
-    private DepthOfField _dof;
 
     private GameController _controller;
 
@@ -21,34 +18,9 @@ public class PostFXHandler : MonoBehaviour {
     private float _targetAperture;
 
     void Start () {
-        _dof = ScriptableObject.CreateInstance<DepthOfField>();
-        _dof.enabled.Override(true);
-        _dof.focusDistance.Override(.1f);
-        _dof.focalLength.Override(7f);
-        _dof.kernelSize.Override(KernelSize.VeryLarge);
-        _dof.aperture.Override(_phoneAperture);
-
-        _volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, _dof);
-        _volume.isGlobal = true;
-
         _controller = this.Find<GameController>(GameTags.GameController);
         _controller.StartGame.AddListener(() => StartCoroutine(SpeedUp()));
 	}
-	
-
-	void Update () {
-        _targetAperture = (GameController.IsUsingPhone || GameController.IsIntro) ? _phoneAperture : _normalAperture;
-
-        _dof.aperture.value = Mathf.Lerp(
-            _dof.aperture.value,
-            _targetAperture,
-            _delta);
-	}
-
-    private void OnDestroy()
-    {
-        RuntimeUtilities.DestroyVolume(_volume, true);
-    }
 
     IEnumerator SpeedUp()
     {
